97ac356adca29a81af492606ab48b0baa876e724
Michael Higginis <mdhiggins23@gmail.com>
2017-12-27 23:20:39 -0500

    eliminate hextolongint
    Not needed using strtoul

--- IRController/IRController.ino
+++ IRController/IRController.ino
@@ -1223,40 +1223,12 @@ String bin2hex(const uint8_t* bin, const int length) {
 
 
 //+=============================================================================
-// Convert string to hex, borrowed from ESPBasic
-//
-unsigned long HexToLongInt(String h)
-{
-  // this function replace the strtol as this function is not able to handle hex numbers greather than 7fffffff
-  // I'll take char by char converting from hex to char then shifting 4 bits at the time
-  int i;
-  unsigned long tmp = 0;
-  unsigned char c;
-  int s = 0;
-  h.toUpperCase();
-  for (i = h.length() - 1; i >= 0 ; i--)
-  {
-    // take the char starting from the right
-    c = h[i];
-    // convert from hex to int
-    c = c - '0';
-    if (c > 9)
-      c = c - 7;
-    // add and shift of 4 bits per each char
-    tmp += c << s;
-    s += 4;
-  }
-  return tmp;
-}
-
-
-//+=============================================================================
 // Send IR codes to variety of sources
 //
 void irblast(String type, String dataStr, unsigned int len, int rdelay, int pulse, int pdelay, int repeat, long address, IRsend irsend) {
   Serial.println("Blasting off");
   type.toLowerCase();
-  unsigned long data = HexToLongInt(dataStr);
+  unsigned long data = strtoul(("0x" + dataStr).c_str(), 0, 0);
   holdReceive = true;
   Serial.println("Blocking incoming IR signals");
   // Repeat Loop
