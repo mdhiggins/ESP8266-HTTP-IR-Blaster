15c2c2ee71dc5b093ec8293bd30ad25403b23c05
Michael Higgins <mdhiggins23@gmail.com>
2018-10-23 09:41:08 -0400

    new time library
    ntpClient was having too many problems and not reliable, gonna try Time

--- src/IRController.ino
+++ src/IRController.ino
@@ -15,14 +15,14 @@
 #include "sha256.h"
 
 #include <Ticker.h>                                           // For LED status
-#include <NTPClient.h>
+#include <TimeLib.h>
 
 // User settings are below here
 
 const bool getExternalIP = true;                              // Set to false to disable querying external IP
 
 const bool getTime = true;                                    // Set to false to disable querying for the time
-const int timeOffset = -14400;                                // Timezone offset in seconds
+const int timeZone = -5;                                      // Timezone (-5 is EST)
 
 const bool enableMDNSServices = true;                         // Use mDNS services, must be enabled for ArduinoOTA
 
@@ -66,12 +66,12 @@ IRsend irsend2(pins2);
 IRsend irsend3(pins3);
 IRsend irsend4(pins4);
 
-const unsigned long resetfrequency = 259200000;                // 72 hours in milliseconds
-//const char* poolServerName = "pool.ntp.org";
-const char* poolServerName = "time.google.com";
-
+const unsigned long resetfrequency = 259200000;                // 72 hours in milliseconds for external IP reset
+static const char ntpServerName[] = "time.google.com";
+unsigned int localPort = 8888;                                 // Local port to listen for UDP packets
+void sendNTPpacket(IPAddress &address);
+time_t getNtpTime();
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, poolServerName, timeOffset);
 
 char _ip[16] = "";
 
@@ -185,7 +185,7 @@ bool validateHMAC(String epid, String mid, String timestamp, String signature) {
     userIDError = !(validUID(user_id));
 
     time_t timethen = timestamp.toInt();
-    time_t timenow = timeClient.getEpochTime() - timeOffset;
+    time_t timenow = now() - (timeZone * SECS_PER_HOUR);
     time_t timediff = abs(timethen - timenow);
     if (timediff > 30) {
       Serial.println("Failed security check, signature is too old");
@@ -821,7 +821,15 @@ void setup() {
   server->begin();
   Serial.println("HTTP Server started on port " + String(port));
 
-  timeClient.begin();
+
+  Serial.println("Starting UDP");
+  ntpUDP.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(ntpUDP.localPort());
+  Serial.println("Waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
+  
   externalIP();
 
   if (strlen(user_id) > 0) {
@@ -831,7 +839,7 @@ void setup() {
     }
 
     // Validation check time
-    time_t timenow = timeClient.getEpochTime() - timeOffset;
+    time_t timenow = now() - (timeZone * SECS_PER_HOUR);
     bool validEpoch = validEPOCH(timenow);
     if (validEpoch) {
       Serial.println("EPOCH time obtained for security checks");
@@ -846,6 +854,70 @@ void setup() {
   irsend4.begin();
   irrecv.enableIRIn();
   Serial.println("Ready to send and receive IR signals");
+}
+
+
+//+=============================================================================
+// NTP Code
+//
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (ntpUDP.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = ntpUDP.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      ntpUDP.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+
+//+=============================================================================
+// Send an NTP request to the time server at the given address
+//
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  ntpUDP.beginPacket(address, 123); //NTP requests are to port 123
+  ntpUDP.write(packetBuffer, NTP_PACKET_SIZE);
+  ntpUDP.endPacket();
 }
 
 //+=============================================================================
@@ -871,7 +943,7 @@ int rokuCommand(String ip, String data, int repeat, int rdelay) {
     last_send.bits = 1;
     strncpy(last_send.encoding, "roku", 14);
     strncpy(last_send.address, ip.c_str(), 20);
-    last_send.timestamp = timeClient.getEpochTime();
+    last_send.timestamp = now();
     last_send.valid = true;
   
     output = http.POST("");
@@ -1007,9 +1079,9 @@ void sendHeader(int httpcode) {
 // Send footer HTML
 //
 void sendFooter() {
-  server->sendContent("      <div class='row'><div class='col-md-12'><em>" + String(millis()) + "ms uptime; EPOCH " + String(timeClient.getEpochTime() - timeOffset) + "</em> / <em id='jepoch'></em> ( <em id='jdiff'></em> )</div></div>\n");
+  server->sendContent("      <div class='row'><div class='col-md-12'><em>" + String(millis()) + "ms uptime; EPOCH " + String(now() - (timeZone * SECS_PER_HOUR)) + "</em> / <em id='jepoch'></em> ( <em id='jdiff'></em> )</div></div>\n");
   server->sendContent("      <script>document.getElementById('jepoch').innerHTML = Math.round((new Date()).getTime() / 1000)</script>");
-  server->sendContent("      <script>document.getElementById('jdiff').innerHTML = Math.abs(Math.round((new Date()).getTime() / 1000) - " + String(timeClient.getEpochTime() - timeOffset) + ")</script>");
+  server->sendContent("      <script>document.getElementById('jdiff').innerHTML = Math.abs(Math.round((new Date()).getTime() / 1000) - " + String(now() - (timeZone * SECS_PER_HOUR)) + ")</script>");
   if (strlen(user_id) != 0)
   server->sendContent("      <div class='row'><div class='col-md-12'><em>Device secured with SHA256 authentication. Only commands sent and verified with Amazon Alexa and the IR Controller Skill will be processed</em></div></div>");
   if (authError)
@@ -1018,7 +1090,7 @@ void sendFooter() {
   server->sendContent("      <div class='row'><div class='col-md-12'><em>Error - last authentication failed because your timestamps are out of sync, see serial output for debugging details. Timediff: " + String(timeAuthError) + "</em></div></div>");
   if (externalIPError)
   server->sendContent("      <div class='row'><div class='col-md-12'><em>Error - unable to retrieve external IP address, this may be due to bad network settings. There is currently a bug with the latest versions of ESP8266 for Arduino, please use version 2.4.0 along with lwIP v1.4 Prebuilt to resolve this</em></div></div>");
-  time_t timenow = timeClient.getEpochTime() - timeOffset;
+  time_t timenow = now() - (timeZone * SECS_PER_HOUR);
   if (!validEPOCH(timenow))
   server->sendContent("      <div class='row'><div class='col-md-12'><em>Error - EPOCH time is inappropraitely low, likely connection to external time server has failed, check your network settings</em></div></div>");
   if (userIDError)
@@ -1414,7 +1486,7 @@ void irblast(String type, String dataStr, unsigned int len, int rdelay, int puls
   last_send.bits = len;
   strncpy(last_send.encoding, type.c_str(), 14);
   strncpy(last_send.address, ("0x" + String(address, HEX)).c_str(), 20);
-  last_send.timestamp = timeClient.getEpochTime();
+  last_send.timestamp = now();
   last_send.valid = true;
 
   resetReceive();
@@ -1451,7 +1523,7 @@ void pronto(JsonArray &pronto, int rdelay, int pulse, int pdelay, int repeat, IR
   last_send.bits = psize;
   strncpy(last_send.encoding, "PRONTO", 14);
   strncpy(last_send.address, "0x0", 20);
-  last_send.timestamp = timeClient.getEpochTime();
+  last_send.timestamp = now();
   last_send.valid = true;
 
   resetReceive();
@@ -1489,7 +1561,7 @@ void rawblast(JsonArray &raw, int khz, int rdelay, int pulse, int pdelay, int re
   last_send.bits = raw.size();
   strncpy(last_send.encoding, "RAW", 14);
   strncpy(last_send.address, "0x0", 20);
-  last_send.timestamp = timeClient.getEpochTime();
+  last_send.timestamp = now();
   last_send.valid = true;
 
   resetReceive();
@@ -1545,7 +1617,6 @@ void copyCode (Code& c1, Code& c2) {
 }
 
 void loop() {
-  timeClient.update();
   ArduinoOTA.handle();
   server->handleClient();
   decode_results  results;                                        // Somewhere to store the results
@@ -1559,7 +1630,7 @@ void loop() {
     copyCode(last_recv_2, last_recv_3);                           // Pass
     copyCode(last_recv, last_recv_2);                             // Pass
     cvrtCode(last_recv, &results);                                // Store the results
-    last_recv.timestamp = timeClient.getEpochTime();               // Set the new update time
+    last_recv.timestamp = now();                                  // Set the new update time
     last_recv.valid = true;
     Serial.println("");                                           // Blank line between entries
     irrecv.resume();                                              // Prepare for the next value
