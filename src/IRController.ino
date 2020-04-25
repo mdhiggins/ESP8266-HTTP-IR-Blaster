90ca42b6bf8a24e80818de375285f7023d21fa7d
Michael Higgins <mdhiggins23@gmail.com>
2020-04-25 14:14:20 -0400

    expand raw memory and include json parsing error

--- src/IRController.ino
+++ src/IRController.ino
@@ -573,7 +573,7 @@ void setup() {
   server->on("/json", []() { // JSON handler for more complicated IR blaster routines
     Serial.println("Connection received - JSON");
 
-    DynamicJsonDocument root(1024);
+    DynamicJsonDocument root(4096);
     DeserializationError error = deserializeJson(root, server->arg("plain"));
 
     int simple = 0;
@@ -586,9 +586,10 @@ void setup() {
 
     if (error) {
       Serial.println("JSON parsing failed");
+      Serial.println(error.c_str());
       if (simple) {
         sendCorsHeaders();
-        server->send(400, "text/plain", "JSON parsing failed");
+        server->send(400, "text/plain", "JSON parsing failed, " + String(error.c_str()));
       } else {
         sendHomePage("JSON parsing failed", "Error", 3, 400); // 400
       }
